13fb955cfe4b52c3c83972ed1ab08e9fbc2d124e
Vova <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>
2022-07-20 22:34:49 +0300

    created function to get all book from dummy data

--- main.py
+++ main.py
@@ -1,8 +1,43 @@
+from typing import List
 from fastapi import FastAPI
 
+from models import Book, BookGenres, BookType
+
 app = FastAPI()
+
+db: List[Book] = [
+    Book(
+        id=1,
+        title='First book',
+        description='description for book',
+        year=2000,
+        ISBN='978-966-2355-57-4',
+        pages=200,
+        author=['Jonh Doe', 'Jane doe'],
+        genre=BookGenres.horror,
+        type=BookType.paper,
+        reviews=[]
+    ),
+    Book(
+        id=2,
+        title='Second book',
+        description='description for new book',
+        year=1998,
+        ISBN='978-961-2312-57-3',
+        pages=400,
+        author=['Bob Cat'],
+        genre=BookGenres.comic_book,
+        type=BookType.paper,
+        reviews=['this is cool']
+    )
+]
 
 
 @app.get("/")
 def root():
     return {"message": "Hello World"}
+
+
+@app.get("/books/")
+def get_all_books():
+    return db
