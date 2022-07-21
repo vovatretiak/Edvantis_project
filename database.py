7c020d61953bb1fd7b5181d28550bd4d847b71bc
Vova <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>
2022-07-21 10:19:44 +0300

    created SQLAlchemy parts for database

---
+++ database.py
@@ -0,0 +1,21 @@
+import os
+
+from dotenv import find_dotenv, load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+load_dotenv(find_dotenv())
+
+DB_USER = 'postgres'
+DB_PASS = os.environ.get('pass')
+DB_HOST = 'localhost'
+DB_PORT = '5432'
+DB_NAME = "edvantis_project"
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
