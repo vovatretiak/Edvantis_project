42021b229aec6a496e7aaced40f48c467098cdd2
Vova <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>
2022-07-27 21:36:02 +0300

    added post function to regiter new user

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 
 from project import models
 from project.database import engine
-from project.routers import authors, books, reviews
+from project.routers import authors, books, reviews, users
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -11,6 +11,7 @@ app = FastAPI()
 app.include_router(authors.router)
 app.include_router(books.router)
 app.include_router(reviews.router)
+app.include_router(users.router)
 
 
 @app.get("/")
---
+++ project/routers/users.py
@@ -0,0 +1,16 @@
+from typing import List
+
+from fastapi import APIRouter, Depends, status
+from project import crud, database, schemas
+from sqlalchemy.orm import Session
+
+router = APIRouter(prefix="/users", tags=["Users"])
+
+get_db = database.get_db
+
+
+@router.post(
+    "/registration", response_model=schemas.User, status_code=status.HTTP_201_CREATED
+)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    pass
