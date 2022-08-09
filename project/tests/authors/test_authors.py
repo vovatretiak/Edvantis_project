d52ffadd49bf5ab80b30268f18fbc69f381a4445
Vova <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>
2022-08-09 18:04:34 +0300

    added test for get method to show all authors

--- project/tests/authors/test_authors.py
+++ project/tests/authors/test_authors.py
@@ -20,3 +20,26 @@ def test_create_author():
     response_data = response.json()
     assert len(response_data) == 4
     assert author in response_data
+
+
+def test_get_all_authors():
+    """tests get method to show all authors"""
+    response = client.get("/authors/")
+    assert response.status_code == 200
+    authors = response.json()
+    assert len(authors) == 3
+    assert authors[0]["first_name"] == "Averell"
+    assert "id" in authors[0]
+    assert "image_file" in authors[0]
+    assert authors[1]["middle_name"] is None
+    assert authors[1]["id"] == 2
+    assert authors[2]["id"] == 3
+    assert authors[2]["first_name"] is None
+    author = authors[0]
+    # with params
+    response = client.get("/authors/", params={"offset": 1, "limit": 2})
+    assert response.status_code == 200
+    authors = response.json()
+    assert author not in authors
+    assert authors[0]["id"] == 2
+    assert len(authors) == 2
