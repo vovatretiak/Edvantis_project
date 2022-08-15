9c255b25b95dcb9043d4eead1174e5650095165b
Vova <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>
2022-08-15 10:59:27 +0300

    added metadata and changed docs url

--- main.py
+++ main.py
@@ -9,7 +9,62 @@ from project.routers import users
 
 Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
+tags_metadata = [
+    {
+        "name": "Authors",
+        "description": """
+
+Things you can do:
+* **Get all authors**
+* **Create new author**
+* **Get author by id**
+* **Update author by id**
+* **Delete author by id**""",
+    },
+    {
+        "name": "Books",
+        "description": """
+
+Things you can do:
+* **Get all books (available filters by genre and type)**
+* **Create new book**
+* **Get books by specific rating**
+* **Get books recommendations by genre**
+* **Get book by id**
+* **Update book by id**
+* **Delete book by id**
+* **Get books by author id**""",
+    },
+    {
+        "name": "Reviews",
+        "description": """
+Things you can do:
+* **Get all reviews**
+* **Create new review if you authorized**
+* **Get review by id**
+* **Update review by id if you authorized**
+* **Delete review by id if you authorized**""",
+    },
+    {
+        "name": "Users",
+        "description": """
+Things you can do:
+* **Create user**
+* **Login with your credentials**
+* **Get all users**
+* **Get your own profile if you authorized**
+* **Update your own profile if you authorized**
+* **Delete your own profile if you authorized**""",
+    },
+]
+
+app = FastAPI(
+    title="Book Reviews App",
+    openapi_tags=tags_metadata,
+    version="0.0.1",
+    contact={"name": "Volodymyr Tretiak", "email": "vovatretiak97@gmail.com"},
+    docs_url="/",
+)
 
 app.include_router(authors.router)
 app.include_router(books.router)
@@ -17,7 +72,7 @@ app.include_router(reviews.router)
 app.include_router(users.router)
 
 
-@app.get("/")
+@app.get("/root/")
 def root():
     """
     default message
